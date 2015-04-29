ded3eca9f2a19e3285353946567c44b4fcb1e328
Yizhi Wang <yzwang@vt.edu>
2015-04-29 12:42:36 -0400

    test

--- test/test_registration_dff_lr_sec5_all.R
+++ test/test_registration_dff_lr_sec5_all.R
@@ -4,25 +4,14 @@
 ## init ----------
 rm(list=ls())
 source('./init.R')
-# thr0 <- 0.05
-# thr0a <- 0.02
-thr0 <- 0.1
-thr0a <- 0.03
-thr1 <- 0.03
-thr2 <- 0.03
-activity_data <- 2
+activity_data <- 3
 
 ## neuron -----------------
 if(activity_data==1) {  # neuron maxz
     img0 <- readImage(datfp$maxz_EGFP,type = 'tiff',all = T)
     img0 <- channel(img0,'green')
-    img0a <- img0
-    img0a[img0<=thr0] <- 0
-    img0a[img0>thr0] <- 1
-    display(img0a)
     ## heatmap
     xx0 <- img0
-    xx0[xx0<thr0] <- 0
     xx0 <- t(apply(xx0, 1, rev))
     par(mar = c(.5,.5,.5,.5))
     fields::image.plot(xx0,axes=F,col = my_palette,legend.width=0.5)
@@ -34,14 +23,11 @@ if(activity_data==2) {  # neuron 40ap, max(dff)
     f_max <- apply(img,c(1,2),max)
     f_bar0 <- apply(img,c(1,2),mean)
     f_bar <- f_bar0
-    f_bar[f_bar<thr0a] <- 1
+    show(sum(f_bar<0.0001))
+    f_bar[f_bar<0.0001] <- 1
     rt0 <- (f_max-f_bar)/f_bar
-    rt0[rt0<0] <- 0
+    rt0[rt0<0] <- -1
     img0 <- as.Image(rt0/max(rt0))
-    img0a <- img0
-    img0a[img0<=thr0] <- 0
-    img0a[img0>thr0] <- 1
-    display(img0a)
     ## heatmap
     xx0 <- rt0
     xx0[xx0>7] <- 7
@@ -56,15 +42,9 @@ if(activity_data==3) {  # neuron 40ap, f_max-f_mean
     f_max <- apply(img,c(1,2),max)
     f_bar <- apply(img,c(1,2),mean)
     rt0 <- f_max-f_bar
-    rt0[rt0<0] <- 0
-    img0 <- as.Image(rt0/max(rt0))
-    img0a <- img0
-    img0a[img0<=thr0] <- 0
-    img0a[img0>thr0] <- 1
-    display(img0a)
+    img0 <- rt0
     ## heatmap
     xx0 <- img0
-    xx0[xx0<thr0] <- 0
     xx0 <- t(apply(xx0, 1, rev))
     par(mar = c(.5,.5,.5,.5))
     fields::image.plot(xx0,axes=F,col = my_palette,legend.width=0.5)
@@ -72,75 +52,31 @@ if(activity_data==3) {  # neuron 40ap, f_max-f_mean
 }
 
 ## synapse --------------------
-## synapsin
 img1 <- readImage(datfp$synapsin[5],type = 'tiff',all = T)
 img1 <- channel(img1,'gray')
-img1a <- img1
-img1a[img1<=thr1] <- 0
-img1a[img1>thr1] <- 1
 img2 <- readImage(datfp$psd95[5],type = 'tiff',all = T)
 img2 <- channel(img2,'gray')
-img2a <- img2
-img2a[img2<=thr2] <- 0
-img2a[img2>thr2] <- 1
-
-## overlap
-img12a <- img1a * img2a
-img12b <- pmin(img1@.Data,img2@.Data)
-# img12 <- img1 * img2
-# img12 <- img12/max(img12)
-# img12[img0a==0] <- 0
-idx_sel <- which(img0a>0)
-x <- img12b[idx_sel]
-x_bin <- img12a[idx_sel]
-# x <- img12[idx_sel]
-y <- img0[idx_sel]
-
-## heatmaps --------------
-## heatmap for img12
-# xx1 <- t(apply(img12, 1, rev))
-# xx1[xx1>0.7] <- 0.7
-# xx1[xx1<0.001] <- 0
-# par(mar = c(.5,.5,.5,.5))
-# fields::image.plot(xx1,axes=F,col = my_palette,legend.width=0.5)
-# par(mar = c(2.5,2.5,2.5,2.5))
+img12 <- pmin(img1@.Data,img2@.Data)
 
 ## heatmap for img12 binarized
-x_bin1 <- img1a * img2a
-xx2 <- t(apply(x_bin1, 1, rev))
+xx2 <- t(apply(img12, 1, rev))
 par(mar = c(.5,.5,.5,.5))
 fields::image.plot(xx2,axes=F,col = my_palette,legend.width=0.5)
 par(mar = c(2.5,2.5,2.5,2.5))
 
-## compare with xx0
-xx2_bar <- 1-xx2
-xx0_xx2 <- xx0 * xx2
-xx0_xx2_bar <- xx0 * xx2_bar
-par(mar = c(.5,.5,2.5,.5))
-fields::image.plot(xx0_xx2,axes=F,col = my_palette,legend.width=0.5,main='pixels with synapse')
-par(mar = c(2.5,2.5,2.5,2.5))
-par(mar = c(.5,.5,2.5,.5))
-fields::image.plot(xx0_xx2_bar,axes=F,col = my_palette,legend.width=0.5,main='pixels without synapse')
-par(mar = c(2.5,2.5,2.5,2.5))
-
-cat(sum(xx0_xx2>0),'\n')
-cat(sum(xx0_xx2_bar>0),'\n')
-
 ## regression ----------------
-y_log <- log(y+0.1)
-x_log <- log(x+0.1)
-# hist(x_log)
-# hist(y_log)
+idx <- which(img0>-1)
+x <- img12[idx]
+y <- img0[idx]
+y_log <- log(y+0.01)
+x_log <- log(x+0.01)
+hist(x_log)
+hist(y_log)
 
-hist(y_log[x_bin==0],main='Response with x=0')
-hist(y_log[x_bin==1],main='Response with x=1')
-
-# show(summary(lm(y~x)))
+show(summary(lm(y~x)))
 # show(summary(lm(y_log~x)))
 show(summary(lm(y_log~x_log)))
-show(summary(lm(y_log~x_bin)))
-
-par(mar = c(5,5,2.5,2.5))
+par(mar = c(5,5,5,5))
 plot(x_log,y_log,cex=0.1,xlab='log(x)',ylab='log(y)')
 
 
